56003ef08ff6617742e932328f1f5a7eab583af4
Sumit Paul <pauls1317@gmail.com>
2024-04-11 15:54:17 +0530

    assignment op associativity

--- vanilla js/mind-grenades/_15_operator_associativity.js
+++ vanilla js/mind-grenades/_15_operator_associativity.js
@@ -1,4 +1,14 @@
+//* comparison operator
 console.log(1 < 2 < 3); // true -> Left to Right associativity
 
 console.log(3 > 2 > 1); // false -> Left to Right associativity
 // 3 > 2 = true -> true > 1 -> 1 > 1 = false
+
+
+//* assignment operator - Right to Left
+let x = 1, y = 2, z = 3;
+
+x = y
+x = y = z
+
+console.log(x, y, z); // Output: 3 3 3
