ff1dc1c02eec3fb6bc176f0fb2db57782e5c3c24
Sumit Paul <pauls1317@gmail.com>
2024-07-20 21:21:17 +0530

    parameter mutation

--- vanilla js/mind-grenades/_16_function_arguments.js
+++ vanilla js/mind-grenades/_16_function_arguments.js
@@ -1,7 +1,8 @@
+//* mutating array param inside function
 function mutateArray(arr) {
-    arr.push(5);
-    arr = 1;
-    return arr;
+  arr.push(5);
+  arr = 1;
+  return arr;
 }
 
 let list = [1, 2, 3, 4];
@@ -10,3 +11,16 @@ console.log(list); // [1, 2, 3, 4, 5]
 
 list = mutateArray(list);
 console.log(list); // 1
+
+//* mutating object param inside function
+function changeParam(obj) {
+  obj.age = 23;
+  console.log(arguments[0]); // { namee: 'ajay', age: 23 }
+  return obj;
+}
+
+let val = { namee: "ajay" };
+
+console.log(changeParam(val)); // { namee: 'ajay', age: 23 }
+
+console.log(val); // { namee: 'ajay', age: 23 }
