a7546a8adf10df6d2ec73c15a1f90112f3994603
Sumit Paul <pauls1317@gmail.com>
2024-08-15 16:43:13 +0530

    comparing string numbers

--- vanilla js/mind-grenades/_13_comparison.js
+++ vanilla js/mind-grenades/_13_comparison.js
@@ -16,3 +16,16 @@ console.log(0 == 2*0); // true
 
 console.log(JSON.stringify('JS') === 'JS'); // false
 // stringify adds an additional string quote
+
+
+console.log("2" > "12"); // true - compares based on ASCII value of first character
+console.log("0".charCodeAt(0), "1".charCodeAt(0), "2".charCodeAt(0)); // 48, 49, 50
+
+console.log("24" > "13"); // true
+console.log("03" > "12"); // false
+
+//* ASCII Values
+console.log(`0-9: ${"0".charCodeAt(0)}-${"9".charCodeAt(0)}`); // 0-9: 48-57
+console.log(`A-Z: ${"A".charCodeAt(0)}-${"Z".charCodeAt(0)}`); // A-Z: 65-90
+console.log(`a-z: ${"a".charCodeAt(0)}-${"z".charCodeAt(0)}`); // a-z: 97-122
+
