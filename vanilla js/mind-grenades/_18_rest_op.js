1599f4824ff532541643e1a37d04947634ba7fca
Sumit Paul <pauls1317@gmail.com>
2024-08-15 16:26:18 +0530

    renamed folders

---
+++ vanilla js/mind-grenades/_18_rest_op.js
@@ -0,0 +1,16 @@
+// Rest Operator
+
+function myFunc(...args) {
+  console.log(args); // [ 'hey', [ 232323, 2434, 2423 ], '2423 {}' ]
+  console.log(arguments); // [Arguments] { '0': 'hey', '1': [ 232323, 2434, 2423 ], '2': '2423 {}' }
+
+  console.log(typeof args); // object
+  console.log(typeof args); // object
+
+  console.log(Array.isArray(args)); // true
+  console.log(Array.isArray(arguments)); // false
+}
+
+myFunc("hey", [232323, 2434, 2423], "2423 {}");
+
+console.log(typeof myFunc); // function
