57abf71301ac123161c1a231ed3c6f8a4fef5814
Sumit Paul <pauls1317@gmail.com>
2024-08-18 20:21:57 +0530

    Generators in JS

--- vanilla js/topics/_06_generators/index.js
+++ vanilla js/topics/_06_generators/index.js
@@ -1,3 +1,5 @@
+// In JavaScript an iterator is an object which defines a sequence and potentially a return value upon its termination.
+
 function* numbers() {
   yield 1;
   yield 2;
@@ -31,3 +33,23 @@ console.log(iterator.next()); // {value: 1, done: false} -> IMP
 // for (let value of iterator) {
 //   console.log(value);
 // }
+
+function* generateId() {
+  let id = 1;
+  while (true) {
+    const inc = yield id; // returns parameter passed to next()
+    if (inc) {
+      id += inc;
+    } else {
+      id++;
+    }
+  }
+}
+
+const id = generateId();
+console.log(id.next()); // {value: 1, done: true}
+console.log(id.next(5)); // {value: 6, done: true}
+console.log(id.next(10)); // {value: 16, done: true} - yield returns this value passed to next()
+
+// Generators have return method - returns the given value and finishes the generator itself
+console.log(id.return(10)); // {value: 10, done: true}
