547fc87cb97cfe46cd95d6ba0b8eb7ec51d0af81
Sumit Paul <pauls1317@gmail.com>
2024-08-20 23:05:38 +0530

    flatArray problem

---
+++ vanilla js/problems/_05_flatArray.js
@@ -0,0 +1,19 @@
+//** In this implementation order doesn't matter */
+
+function flatArray(arr) {
+  let stack = [...arr];
+  let res = [];
+
+  while (stack.length) {
+    let item = stack.pop();
+    if (Array.isArray(item)) stack.push(...item);
+    else res.push(item);
+  }
+  return res;
+}
+
+console.log(flatArray([1, [2, [3, 4, [5]]]])); // [ 5, 4, 3, 2, 1 ]
+console.log(flatArray([1, [2, [3, 4, [5], 6, 7], 8], 9])); // [ 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
+
+//** Using recursion */
+function flatArray2(arr) {}
