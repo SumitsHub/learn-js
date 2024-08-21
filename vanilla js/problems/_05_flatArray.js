b1148bcc7b652053c75972a73be640d8d1f6c200
Sumit Paul <pauls1317@gmail.com>
2024-08-21 09:24:28 +0530

    flatArray using recursion

--- vanilla js/problems/_05_flatArray.js
+++ vanilla js/problems/_05_flatArray.js
@@ -16,4 +16,18 @@ console.log(flatArray([1, [2, [3, 4, [5]]]])); // [ 5, 4, 3, 2, 1 ]
 console.log(flatArray([1, [2, [3, 4, [5], 6, 7], 8], 9])); // [ 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
 
 //** Using recursion */
-function flatArray2(arr) {}
+function flatArray2(inputArr) {
+  let result = [];
+
+  for (let arr of inputArr) {
+    if (Array.isArray(arr)) {
+      result.push(...flatArray2(arr));
+    } else {
+      result.push(arr);
+    }
+  }
+  return result;
+}
+
+console.log(flatArray2([1, [2, [3, 4, [5]]]])); // [ 1, 2, 3, 4, 5 ]
+console.log(flatArray2([1, [2, [3, 4, [5], 6, 7], 8], 9])); // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
