4a2b95777bb8449874154caaba954bdc286bfb78
Sumit Paul <pauls1317@gmail.com>
2024-09-02 21:36:53 +0530

    flat array with level

--- vanilla js/problems/_05_flatArray.js
+++ vanilla js/problems/_05_flatArray.js
@@ -31,3 +31,26 @@ function flatArray2(inputArr) {
 
 console.log(flatArray2([1, [2, [3, 4, [5]]]])); // [ 1, 2, 3, 4, 5 ]
 console.log(flatArray2([1, [2, [3, 4, [5], 6, 7], 8], 9])); // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+
+//** Flat array with level of nesting */
+
+function flat(arr, level = 1) {
+
+  if (level < 1) return arr;
+
+  return arr.reduce((acc, cur)=>{  
+    if (Array.isArray(cur)) {
+      acc.push(...flat(cur, level - 1));
+    } else {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
+}
+
+const nestedArray = [1, [2, 3], [4, [5, 6]]];
+console.log(flat(nestedArray)); // [ 1, 2, 3, 4, [ 5, 6 ] ]
+console.log(flat(nestedArray, 1)); // [ 1, 2, 3, 4, [ 5, 6 ] ]
+console.log(flat(nestedArray, 2)); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(flat(nestedArray, Infinity)); // [ 1, 2, 3, 4, 5, 6 ]
