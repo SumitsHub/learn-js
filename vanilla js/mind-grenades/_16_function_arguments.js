878bd9482b80a98b78c96238b4ae306ebf1d3810
Sumit Paul <pauls1317@gmail.com>
2024-11-13 09:41:25 +0530

    modify arguments keyword inside function

--- vanilla js/mind-grenades/_16_function_arguments.js
+++ vanilla js/mind-grenades/_16_function_arguments.js
@@ -1,4 +1,4 @@
-//* mutating array param inside function
+//* #01 - mutating array param inside function
 function mutateArray(arr) {
   arr.push(5);
   arr = 1;
@@ -12,7 +12,7 @@ console.log(list); // [1, 2, 3, 4, 5]
 list = mutateArray(list);
 console.log(list); // 1
 
-//* mutating object param inside function
+//* #02 - mutating object param inside function
 function changeParam(obj) {
   obj.age = 23;
   console.log(arguments[0]); // { namee: 'ajay', age: 23 }
@@ -24,3 +24,31 @@ let val = { namee: "ajay" };
 console.log(changeParam(val)); // { namee: 'ajay', age: 23 }
 
 console.log(val); // { namee: 'ajay', age: 23 }
+
+//* #03 - changing 'arguments' keyword inside function
+/** non strict mode - modifying 'arguments' keyword is allowed */
+function test(obj) {
+  arguments[0].age = 12;
+  console.log(arguments[0]);
+}
+
+let ob = {};
+test(ob);
+console.log(ob);
+
+// { age: 12 }
+// { age: 12 }
+
+/** strict mode - modifying 'arguments' keyword is allowed */
+("use strict");
+function test2(obj) {
+  arguments[0].age = 123;
+  console.log(arguments[0]);
+}
+
+let ob2 = {};
+test2(ob2);
+console.log(ob2);
+
+// { age: 123 }
+// { age: 123 }
