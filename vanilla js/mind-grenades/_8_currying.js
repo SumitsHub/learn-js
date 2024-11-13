559be9da0e2906333612711ef1105dc15bce68ea
Sumit Paul <pauls1317@gmail.com>
2024-11-13 09:24:16 +0530

    currying question revisited

--- vanilla js/mind-grenades/_8_currying.js
+++ vanilla js/mind-grenades/_8_currying.js
@@ -1,8 +1,9 @@
-// function currying
+//* function currying
 // Currying is the process of taking a function with multiple arguments and turning it into a sequence of functions each with only a single argument.
 
 // Curried functions are constructed by chaining closures and by defining and immediately returning their inner functions simultaneously.
 
+//* Problem #01 */
 function add(...args) {
   let sum = args.reduce((acc, val) => acc + val, 0);
 
@@ -14,10 +15,12 @@ function add(...args) {
     return inner;
   }
 
-  return inner(...args);
+  return inner;
 }
 
-console.log(add(1, 2, 3)(2)(2)(2)(3)());
+console.log("res #01", add(1, 2, 3)(2)(2)(2)(3)()); // res #01 15
+console.log("res #02", add(1, 2, 3)()); // res #02 6
+console.log("res #03", add(1, 2, 3)(0)()); // res #03 6
 
 /* 
 In this implementation:
@@ -27,28 +30,37 @@ In this implementation:
 - When the inner function is called, it either adds the provided arguments to the sum or returns the current sum if no arguments are provided.
 */
 
-function add(...args) {
+// Explanation
+// Initial Sum Calculation: The function starts with an initial sum of args.
+// Recursive Accumulation: Each call to inner takes in new arguments, sums them, and adds them to sum.
+// Final Call: When inner() is called with no arguments, it returns the accumulated sum
+
+//* Problem #02 */
+
+function add2(...args) {
   const sum = args.reduce((acc, val) => acc + val, 0);
 
   function addInner(...innerArgs) {
-    return add(...args, ...innerArgs);
+    return add2(...args, ...innerArgs);
   }
 
   addInner.valueOf = function () {
     return sum;
   };
 
+  addInner.toString = addInner.valueOf;
+
   return addInner;
 }
 
 // Usage
-const result1 = add(1, 2, 3)(4, 5);
-const result2 = add(1, 2)(3, 4)(5, 5);
-const result3 = add(1, 2, 3)(4, 5, 6);
+const result1 = add2(1, 2, 3)(4, 5);
+const result2 = add2(1, 2)(3, 4)(5, 5);
+const result3 = add2(1, 2, 3)(4, 5, 6);
 
-console.log(result1); // Output: 15
-console.log(result2); // Output: 20
-console.log(result3); // Output: 21
+console.log(result1.valueOf()); // Output: 15
+console.log(result2.valueOf()); // Output: 20
+console.log(result3.valueOf()); // Output: 21
 
 /* In this implementation:
 
